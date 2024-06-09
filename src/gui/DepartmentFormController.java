8dab242ba8588988dd33976dc58a38905e03f45c
Carlos-GleysonTI <c.gleysonti@gmail.com>
2024-06-09 19:58:00 -0300

    DepartmentFormController

---
+++ src/gui/DepartmentFormController.java
@@ -0,0 +1,54 @@
+package gui;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import gui.util.Constraints;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class DepartmentFormController implements Initializable {
+	
+	@FXML
+	private TextField txtId;
+	
+	@FXML
+	private TextField txtName;
+	
+	@FXML
+	private Label labelError;
+	
+	@FXML
+	private Button btSave;
+	
+	@FXML
+	private Button btCancel;
+	
+	//criar os eventos da botões
+	@FXML
+	public void onBtSaveAction() {
+		System.out.println("onBtSaveAction");
+	}
+	
+	@FXML
+	public void onBtCancelAction() {
+		System.out.println("onBtCancelAction");
+	}
+	
+	
+	@Override
+	public void initialize(URL url, ResourceBundle rb) {
+		initializeNodes(); //chamei a função 
+	}
+	
+	//vamos criar as funções d restrições
+	public void initializeNodes() {
+		Constraints.setTextFieldInteger(txtId);//so aceita numero inteiro
+		Constraints.setTextFieldMaxLength(txtName, 30); // maximo 30 caracteres 
+	}
+
+}
+ 
