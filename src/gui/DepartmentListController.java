584492b5131edbe1c50e7b0c3d7fb4b5670474ce
Carlos-GleysonTI <c.gleysonti@gmail.com>
2024-06-12 19:44:57 -0300

    Update department

--- src/gui/DepartmentListController.java
+++ src/gui/DepartmentListController.java
@@ -1,6 +1,5 @@
 package gui;
 
-
 import java.io.IOException;
 import java.net.URL;
 import java.util.List;
@@ -10,6 +9,7 @@ import application.Main;
 import gui.listeners.DataChangeListener;
 import gui.util.Alerts;
 import gui.util.Utils;
+import javafx.beans.property.ReadOnlyObjectWrapper;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
@@ -19,6 +19,7 @@ import javafx.fxml.Initializable;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
+import javafx.scene.control.TableCell;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
@@ -28,25 +29,28 @@ import javafx.stage.Stage;
 import model.entities.Department;
 import model.services.DepartmentService;
 
-public class DepartmentListController implements Initializable, DataChangeListener{
-	
-	//injetando
+public class DepartmentListController implements Initializable, DataChangeListener {
+
+	// injetando
 	private DepartmentService service;
-	
+
 	@FXML
 	private TableView<Department> tableViewDepartment;
-	
+
 	@FXML
 	private TableColumn<Department, Integer> tableColumnId;
-	
+
 	@FXML
 	private TableColumn<Department, String> tableColumnName;
-	
+
+	@FXML
+	private TableColumn<Department, Department> tableColumnEDIT;
+
 	@FXML
 	private Button btNew;
-	
+
 	private ObservableList<Department> obsList;
-	
+
 	@FXML
 	public void onBtNewAction(ActionEvent event) {
 		Stage parentStage = Utils.currentStage(event);
@@ -57,7 +61,7 @@ public class DepartmentListController implements Initializable, DataChangeListen
 	public void setDepartmentService(DepartmentService service) {
 		this.service = service;
 	}
-	
+
 	@Override
 	public void initialize(URL url, ResourceBundle rb) {
 		initializeNodes();
@@ -66,12 +70,12 @@ public class DepartmentListController implements Initializable, DataChangeListen
 	private void initializeNodes() {
 		tableColumnId.setCellValueFactory(new PropertyValueFactory<>("id"));
 		tableColumnName.setCellValueFactory(new PropertyValueFactory<>("name"));
-		
-		//deixar a TableView na tela toda
+
+		// deixar a TableView na tela toda
 		Stage stage = (Stage) Main.getMainScene().getWindow();
 		tableViewDepartment.prefHeightProperty().bind(stage.heightProperty());
 	}
-	
+
 	public void updateTableView() {
 		if (service == null) {
 			throw new IllegalStateException("Service was null");
@@ -79,23 +83,26 @@ public class DepartmentListController implements Initializable, DataChangeListen
 		List<Department> list = service.findAll();
 		obsList = FXCollections.observableArrayList(list);
 		tableViewDepartment.setItems(obsList);
+		
+		//chamar metodo p atulizar
+		initEditButtons();
 	}
-	
-	//Função p abrir a janela de formualário
+
+	// Função p abrir a janela de formualário
 	private void createDialogForm(Department obj, String absoluteName, Stage parentStage) {
-	
-		try {//carrega a tela
+
+		try {// carrega a tela
 			FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName));
 			Pane pane = loader.load();
-			
-			//pegar o controlador
+
+			// pegar o controlador
 			DepartmentFormController controller = loader.getController();
-			controller.setDepartment(obj);//obj e minha entidade Departamento
-			controller.setDepartmentService(new DepartmentService()); //injetar ele aqui também
-			controller.subscribeDataChangeListener(this);//se escrevendo p receber o evento de noticação
-			controller.updateFormData();//carrego os dados obj(entidade) para o Formulário
-		
-			//Vamos instanciar o stage
+			controller.setDepartment(obj);// obj e minha entidade Departamento
+			controller.setDepartmentService(new DepartmentService()); // injetar ele aqui também
+			controller.subscribeDataChangeListener(this);// se escrevendo p receber o evento de noticação
+			controller.updateFormData();// carrego os dados obj(entidade) para o Formulário
+
+			// Vamos instanciar o stage
 			Stage dialogStage = new Stage();
 			dialogStage.setTitle("Enter Department data");
 			dialogStage.setScene(new Scene(pane));
@@ -103,15 +110,36 @@ public class DepartmentListController implements Initializable, DataChangeListen
 			dialogStage.initOwner(parentStage);
 			dialogStage.initModality(Modality.WINDOW_MODAL);
 			dialogStage.showAndWait();
-		}
-		catch (IOException e) {
+		} catch (IOException e) {
 			Alerts.showAlert("IO Exception", "Error loading view", e.getMessage(), AlertType.ERROR);
-			}
+		}
 	}
 
 	@Override
 	public void onDataChanged() {
 		// TODO Auto-generated method stub
 		updateTableView();
+	}
+
+	// para atudar dados c butão
+	private void initEditButtons() {
+		tableColumnEDIT.setCellValueFactory(param -> new ReadOnlyObjectWrapper<>(param.getValue()));
+		tableColumnEDIT.setCellFactory(param -> new TableCell<Department, Department>() {
+			private final Button button = new Button("edit");
+
+			@Override
+			protected void updateItem(Department obj, boolean empty) {
+				super.updateItem(obj, empty);
+
+				if (obj == null) {
+					setGraphic(null);
+					return;
+				}
+
+				setGraphic(button);
+				button.setOnAction(
+						event -> createDialogForm(obj, "/gui/DepartmentForm.fxml", Utils.currentStage(event)));
+			}
+		});
 	}
 }
