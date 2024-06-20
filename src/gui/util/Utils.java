bc2f9b4364c6559dfc50df7e08e703e5bb518903
Carlos-GleysonTI <c.gleysonti@gmail.com>
2024-06-20 19:11:14 -0300

    Seller TableView

--- src/gui/util/Utils.java
+++ src/gui/util/Utils.java
@@ -1,25 +1,71 @@
 package gui.util;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
 
 import javafx.event.ActionEvent;
 import javafx.scene.Node;
+import javafx.scene.control.TableCell;
+import javafx.scene.control.TableColumn;
 import javafx.stage.Stage;
 
 public class Utils {
 
-	public static Stage currentStage(ActionEvent event) { //vamos ter uma ação com button
-		
-		//vamos criar ação
+	public static Stage currentStage(ActionEvent event) { // vamos ter uma ação com button
+
+		// vamos criar ação
 		return (Stage) ((Node) event.getSource()).getScene().getWindow();
-			//(downcast ) é quando o objeto se passa como se fosse um subtipo dele.
+		// (downcast ) é quando o objeto se passa como se fosse um subtipo dele.
 	}
-	
+
 	public static Integer tryParseToInt(String str) {
 		try {
-			return Integer.parseInt(str); //convertendo
-		}
-		catch(NumberFormatException e) {
+			return Integer.parseInt(str); // convertendo
+		} catch (NumberFormatException e) {
 			return null;
 		}
+	}
+
+	// formatar a data
+	public static <T> void formatTableColumnDate(TableColumn<T, Date> tableColumn, String format) {
+		tableColumn.setCellFactory(column -> {
+			TableCell<T, Date> cell = new TableCell<T, Date>() {
+				private SimpleDateFormat sdf = new SimpleDateFormat(format);
+
+				@Override
+				protected void updateItem(Date item, boolean empty) {
+					super.updateItem(item, empty);
+					if (empty) {
+						setText(null);
+					} else {
+						setText(sdf.format(item));
+					}
+				}
+			};
+
+			return cell;
+		});
+	}
+
+	//formatar ponto flutuante
+	public static <T> void formatTableColumnDouble(TableColumn<T, Double> tableColumn, int decimalPlaces) {
+		tableColumn.setCellFactory(column -> {
+			TableCell<T, Double> cell = new TableCell<T, Double>() {
+
+				@Override
+				protected void updateItem(Double item, boolean empty) {
+					super.updateItem(item, empty);
+					if (empty) {
+						setText(null);
+					} else {
+						Locale.setDefault(Locale.US);
+						setText(String.format("%." + decimalPlaces + "f", item));
+					}
+				}
+			};
+
+			return cell;
+		});
 	}
 }
